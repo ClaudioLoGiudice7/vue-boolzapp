1b11cd362ff9ac41f85850fd96b681cd6106eefa
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-02-16 17:44:34 +0100

    milestone 4 try

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
 
 // Milestone 4
 // ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
-// contatti il cui nome contiene le lettere inserite(es, Marco, Matteo Martina -> Scrivo
+// contatti il cui nome contiene le lettere inserite(es, Marco, Matteo, Martina -> Scrivo
 // “mar” rimangono solo Marco e Martina).
 
 
@@ -229,6 +229,9 @@ const app = Vue.createApp({
 
             // MESSAGGIO AGGIUNTO
             newMessage: "",
+
+            // RICERCA UTENTE
+            searchUser: "",
         }
     },
 
@@ -257,13 +260,24 @@ const app = Vue.createApp({
                 }
                 this.contacts[this.activeChat].messages.push(replyMessage)
             }, 2000);
+        },
+
+        searchName() {
+            for (const contact of this.contacts) {
+                if (
+                    !contact.name.toLowerCase().includes(this.searchUser.toLowerCase())
+                ) {
+                    contact.visible = false;
+                } else {
+                    contact.visible = true;
+                }
+            }
         }
     },
 })
 app.mount("#app");
 
-// Milestone 3
-// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
-// un “ok” come risposta, che apparirà dopo 1 secondo.
+// Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite(es, Marco, Matteo, Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina).
